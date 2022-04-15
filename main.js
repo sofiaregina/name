9cb321e77384ccfc5fa122acccaba839f9b0da5e
sofiaregina <101475968+sofiaregina@users.noreply.github.com>
2022-04-15 20:44:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+nameOfTheLogan= [];
+function logan()
+{
+var images =
+["53d62cd3-8da5-4819-9a07-02602c0d5f67.jpg,logan.jpg.jpg,loganzinho.jpg.jpg,meu baby.jpg.jpg,pose mais fofa.jpg.jpg,lolo.jpg.jpg"];
+var i = 0;
+function update()
+{
+   i++;
+   var numbersOfCatInArray = 5
+   if(i > numbersOfCatInArray );
+}
+{
+   i = 0;
+}
+var updatedImage = images[i];
+
+
+var updatedName = names[i];
+
+
+document.getElementById("familyMemberImage").src=updatedImage;
+document.getElementById("familyMemberImage").innerHTML=updatedName;
+}
